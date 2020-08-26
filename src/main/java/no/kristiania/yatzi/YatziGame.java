4233ef9c8e2d010bbf0b9f7ccd5e3289ae9e1a94
LaurentBaj <laurent_baj@hotmail.com>
2020-08-26 18:51:07 +0200

    Fixed src-code

--- src/main/java/no/kristiania/yatzi/YatziGame.java
+++ src/main/java/no/kristiania/yatzi/YatziGame.java
@@ -4,10 +4,10 @@ public class YatziGame {
     public int score(YatzyCategory category, int[] dice) {
         int result = 0;
         for (int die: dice) {
-            if (die == 1) {
-                result += 1;
-            } else if (die == 2) {
-                result += 2;
+            if (die == 1 && category == YatzyCategory.ONES) {
+                result += die;
+            } else if (die == 2 && category == YatzyCategory.TWOS) {
+                result += die;
             }
         }
         return result;
