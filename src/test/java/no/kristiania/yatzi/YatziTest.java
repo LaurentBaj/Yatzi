98a68a39caae387f27f788f71643c26bb55a762c
LaurentBaj <laurent_baj@hotmail.com>
2020-08-26 16:57:26 +0200

    ONES green, TWOS red

--- src/test/java/no/kristiania/yatzi/YatziTest.java
+++ src/test/java/no/kristiania/yatzi/YatziTest.java
@@ -12,7 +12,14 @@ public class YatziTest {
     void shouldScoreOnes() {
         yatziGame = new YatziGame();
         assertEquals(4, yatziGame.score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1 }));
-        assertEquals(2, yatziGame.score(YatzyCategory.ONES, new int[] { 6, 5, 2, 2, 1 }));
+        assertEquals(2, yatziGame.score(YatzyCategory.ONES, new int[] { 6, 5, 2, 1, 1 }));
+    }
+
+    @Test
+    void shouldScoreTwo() {
+        yatziGame = new YatziGame();
+        assertEquals(4, yatziGame.score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1 }));
+        assertEquals(2, yatziGame.score(YatzyCategory.ONES, new int[] { 6, 5, 2, 1, 1 }));
     }
 
 
