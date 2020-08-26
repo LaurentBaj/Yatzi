d42f530c733a3bf03ef19f6804e367561473f91c
LaurentBaj <laurent_baj@hotmail.com>
2020-08-26 17:27:57 +0200

    ONES green, TWOS red

--- src/main/java/no/kristiania/yatzi/YatzyCategory.java
+++ src/main/java/no/kristiania/yatzi/YatzyCategory.java
@@ -1,5 +1,5 @@
 package no.kristiania.yatzi;
 
 public enum YatzyCategory {
-    ONES
+    TWOS, ONES
 }
--- src/test/java/no/kristiania/yatzi/YatziTest.java
+++ src/test/java/no/kristiania/yatzi/YatziTest.java
@@ -10,16 +10,14 @@ public class YatziTest {
 
     @Test
     void shouldScoreOnes() {
-        yatziGame = new YatziGame();
         assertEquals(4, yatziGame.score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1 }));
         assertEquals(2, yatziGame.score(YatzyCategory.ONES, new int[] { 6, 5, 2, 1, 1 }));
     }
 
     @Test
     void shouldScoreTwo() {
-        yatziGame = new YatziGame();
-        assertEquals(4, yatziGame.score(YatzyCategory.ONES, new int[] { 1, 1, 2, 1, 1 }));
-        assertEquals(2, yatziGame.score(YatzyCategory.ONES, new int[] { 6, 5, 2, 1, 1 }));
+        assertEquals(4, yatziGame.score(YatzyCategory.TWOS, new int[] { 1, 1, 2, 2, 1 }));
+        assertEquals(2, yatziGame.score(YatzyCategory.TWOS, new int[] { 6, 5, 2, 1, 1 }));
     }
 
 
